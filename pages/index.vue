f9b978ad77ce7f6a84caab3864989f557c42baa6
vanjasoa <vanjasoa@example.com>
2023-11-13 13:49:25 +0300

    feat: :sparkles: test token

--- pages/index.vue
+++ pages/index.vue
@@ -23,12 +23,15 @@ const { login, logout } = useDirectusAuth();
 const user = useDirectusUser();
 const { token } = useDirectusToken();
 const counter = useCookie('counter')
+const t = useCookie('refesh_token')
+
 counter.value = counter.value || Math.round(Math.random() * 1000)
 
 const onSubmit = async () => {
     try {
         await login({ email: "teddy@mail.com", password: "123456" });
         document.cookie = "Token=" + token.value + ";" + expires + ";path=/";
+        t.value = token.value
         console.log(token.value);
     } catch (e) { }
 };
