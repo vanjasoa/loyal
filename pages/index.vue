d309d8a3cf25fc57d2ec9a0398f0b5fa0dd21824
vanjasoa <vanjasoa@example.com>
2023-11-13 13:43:06 +0300

    feat: :sparkles: test cookie

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,13 @@
 
         <button @click="logout">logout</button>
         <button @click="onSubmit">login</button>
+
+        <div>
+            <h1>Counter: {{ counter || '-' }}</h1>
+            <button @click="counter = null">reset</button>
+            <button @click="counter--">-</button>
+            <button @click="counter++">+</button>
+        </div>
     </div>
 </template>
 
@@ -15,13 +22,15 @@
 const { login, logout } = useDirectusAuth();
 const user = useDirectusUser();
 const { token } = useDirectusToken();
+const counter = useCookie('counter')
+counter.value = counter.value || Math.round(Math.random() * 1000)
 
 const onSubmit = async () => {
-  try {
-    await login({ email: "teddy@mail.com", password: "123456" });
-    document.cookie ="Token=" + token.value + ";" + expires + ";path=/";
-    console.log(token.value);
-  } catch (e) {}
+    try {
+        await login({ email: "teddy@mail.com", password: "123456" });
+        document.cookie = "Token=" + token.value + ";" + expires + ";path=/";
+        console.log(token.value);
+    } catch (e) { }
 };
 
 </script>
